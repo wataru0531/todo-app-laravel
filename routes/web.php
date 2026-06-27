affcb030ecbad99c949a33fbff73d639710e58fe
wataru0531 <obito0531@gmail.com>
2026-06-27 23:50:59 +0900

    postメソッド

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,43 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Http\Request;
+
 // ルーティング定義
+// GETでもらう場合
 Route::get('/', function () {
   return view('welcome'); // → resources/views/welcome.blade.php をレンダリング 
 });
 
 
-
-Route::get("/about", function() {
+// ✅ about
+// フォルダ名 + ファイルの先頭名 ... about.index
+Route::get("/about", function() { 
 	return view("about.index"); // about/index.blade.php をレンダリング
 });
+
+
+// ✅ tasks/create
+// GET
+Route::get("/tasks/create", function(){
+	return view("tasks.create");
+});
+
+// POST
+Route::post("/tasks/create", function(Request $request) {
+	// フォームから値を取得。name属性から
+	// $task = request("task");
+	
+	// $task = $request->input("task");
+
+
+	$validate = $request->validate([
+		"task" => ["required", "max:255"],
+		"deadline" => ["nullable"],
+	]);
+
+	// return "タスクが送信されました";
+	// return $task .  "が送信されました";
+	return $validate["task"] . $validate["deadline"];
+});
+
